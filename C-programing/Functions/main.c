0dfb238a6729d5bddc1336eca373bad06fc46069
mohamedkhaled-a11y <76429154+mohamedkhaled-a11y@users.noreply.github.com>
2023-04-24 00:43:33 +0200

    Functions

---
+++ C-programing/Functions/main.c
@@ -0,0 +1,99 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+/*
+int prime_check(int num){
+
+	int x=num/2;
+	int i;
+	for ( i=2;i<=x;i++)
+	{
+		if (num%i==0) return 0;
+	}
+	return num;
+
+}
+*/
+
+/*int recr_fact(int num)
+{
+int fact=1;
+int count=num;
+for (int i=num;i>=1;i--)
+	{
+		fact*=i;
+	}
+return fact;
+count--;
+if (count>0) recr_fcat(count);
+
+}
+*/
+
+/*
+void reverse()
+{
+char c;
+scanf("%c",&c);
+if (c!="\n");
+{
+	reverse();
+	printf("%c",c);
+}
+}
+*/
+
+/*
+int power(base,exp)
+{
+  
+ if (exp!=0) return(base*power(base,exp-1));
+ else return 1
+
+}
+
+*/
+void main ()
+{
+
+/*	int a,b,j;
+	printf("Enter the two intervals :");
+	fflush(stdout);
+	scanf("%d %d",&a,&b);
+	for (j=a;j<=b;j++){
+		if (prime_check(j)) printf("%d ",prime_check(j));
+	}
+*/
+
+/*int fn;
+printf("Enter positive Enteger :");
+fflush(stdout);
+scanf("%d",&fn);
+printf("%d ",recr_fact(fn));
+
+*/
+
+ /*
+printf("Enter a scentence  :");
+reverse();
+*/
+
+/*
+int base,exp;
+printf("Enter base number :");
+scanf("%d",&base);
+printf("Enter power number(positive integer) :");
+scanf("%d",&exp);
+printf("%d^%d = %d",base,exp,power(base,exp));
+
+*/
+
+}
+
+
+
+
+
+
